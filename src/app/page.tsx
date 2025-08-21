46250547cd1291147718a477f6047c8f0ca0b6e2
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-08-21 09:22:07 +0530

    Update layout and page components for Password Generator functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,105 @@
-import Image from "next/image";
+"use client";
+
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [length, setLength] = useState<number>(8);
+  const [numbersAllowed, setNumbersAllowed] = useState<boolean>(false);
+  const [symbolsAllowed, setSymbolsAllowed] = useState<boolean>(false);
+  const [password, setPassword] = useState<string>("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numbersAllowed) {
+      str += "0123456789";
+    }
+    if (symbolsAllowed) {
+      str += "!@#$%^&*()_+[]{}|;:,.<>?";
+    }
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * str.length);
+      pass += str[randomIndex];
+    }
+    setPassword(pass);
+  }, [length, numbersAllowed, symbolsAllowed]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numbersAllowed, symbolsAllowed, passwordGenerator]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    if (passwordRef.current) {
+      passwordRef.current.select();
+      passwordRef.current.setSelectionRange(0, password.length); // full selection for mobile
+    }
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  return (
+    <div className="bg-slate-900 h-screen p-20">
+      <div className="w-full max-w-md mx-auto shadow-md bg-blue-900 rounded-md p-10">
+        {/* Password Display + Copy */}
+        <div className="flex flex-row">
+          <input
+            type="text"
+            value={password}
+            placeholder="password"
+            ref={passwordRef}
+            readOnly
+            className="bg-white flex-1 text-black px-5 py-2 rounded-md"
+          />
+          <button
+            onClick={copyPasswordToClipboard}
+            className="px-5 py-2 bg-amber-400 ml-2 rounded-md text-white hover:bg-amber-500 active:bg-amber-600"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Copy
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        {/* Length Slider */}
+        <div className="w-full max-w-md mx-auto gap-x-2 flex flex-row ">
+          <input
+            type="range"
+            min={8}
+            max={100}
+            value={length}
+            onChange={(e) => setLength(Number(e.target.value))}
+            className="cursor-pointer mx-5 my-2"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <label className="mx-5 my-2">Length: {length}</label>
+        </div>
+
+        {/* Numbers Allowed */}
+        <div className="w-full max-w-md mx-auto gap-x-2 flex flex-row ">
+          <input
+            type="checkbox"
+            id="numberInput"
+            checked={numbersAllowed}
+            onChange={() => setNumbersAllowed((prev) => !prev)}
+            className="cursor-pointer ml-5 my-2"
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+          <label htmlFor="numberInput" className="my-2">
+            Numbers Allowed
+          </label>
+        </div>
+
+        {/* Symbols Allowed */}
+        <div className="w-full max-w-md mx-auto gap-x-2 flex flex-row ">
+          <input
+            type="checkbox"
+            id="symbolInput"
+            checked={symbolsAllowed}
+            onChange={() => setSymbolsAllowed((prev) => !prev)}
+            className="cursor-pointer ml-5 my-2"
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <label htmlFor="symbolInput" className="my-2">
+            Symbols Allowed
+          </label>
+        </div>
+      </div>
     </div>
   );
 }
